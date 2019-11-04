c36866ce4ef40823ce7be59ac13a5d97110eb6fa
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-11-04 17:40:58 +0100

    Initial commit

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Cors;
+
+namespace WarehouseApi.Controllers
+{
+    [ApiController]
+    [EnableCors("AllowAnyOrigin")]
+    [Route("api/v1/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly ILogger<ItemsController> _logger;
+
+        public ItemsController(ILogger<ItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<String> GetAllItems()
+        {
+
+            return new string[] { "ITEM1", "ITEM2" };
+        }
+    }
+}
