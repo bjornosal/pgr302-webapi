63243b49c8ef3f318d29ee36827973dc9510f679
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-11-04 18:53:52 +0100

    Renamed ItemsController to be named correctly. Added OrdersController.

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Cors;
+using WarehouseApi.Models;
+
+namespace WarehouseApi.Controllers
+{
+    [ApiController]
+    [EnableCors("AllowAnyOrigin")]
+    [Route("api/v1/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Order> GetAllOrders()
+        {
+
+            Order order = new Order();
+            order.items = new Item[] { };
+
+            return new Order[] { order };
+        }
+    }
+}
